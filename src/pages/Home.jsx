e273d8c89ef14bd15de66ec159d0a4559ae53a51
Elias Obregon <elias_97sl@hotmail.com>
2026-07-13 17:41:01 -0300

    Home: cambia subtitulo a 'Todo en indumentaria deportiva'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ export default function Home() {
               Proshop Baradero
             </h1>
             <p className="text-gray-400 text-lg md:text-xl">
-              Tu tienda de confianza
+              Todo en indumentaria deportiva
             </p>
           </div>
         </section>
